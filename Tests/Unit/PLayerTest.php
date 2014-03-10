d8de336f030fa76dacb2cc271935015899775f92
sensorario <sensorario@gmail.com>
2014-03-10 04:47:02 +0100

    Removed unused feature

--- Tests/Unit/PLayerTest.php
+++ Tests/Unit/PLayerTest.php
@@ -3,21 +3,10 @@
 namespace Tests\Unit;
 
 use PHPUnit_Framework_TestCase;
-use Sensorario\Yagolands\Player;
 
 class PlayerTest extends PHPUnit_Framework_TestCase
 {
-    public function testPlayer()
+    public function testNothing()
     {
-        $world = $this->getMockBuilder('Sensorario\Yagolands\World')
-            ->disableOriginalConstructor()
-            ->setMethods(['getName'])
-            ->getMock();
-        $world->expects($this->once())
-            ->method('getName')
-            ->will($this->returnValue('yagolands'));
-
-        $player = new Player($world);
-        $this->assertEquals('yagolands', $player->getWorldName());
     }
 }
